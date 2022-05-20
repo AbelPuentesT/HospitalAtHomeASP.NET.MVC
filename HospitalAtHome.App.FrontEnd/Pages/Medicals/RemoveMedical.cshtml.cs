57c0c42359882d953a1581b8d0ba1b51f5fa4f81
AbelPuentesT <90673923+AbelPuentesT@users.noreply.github.com>
2022-05-19 21:49:13 -0500

    AddAllPages

--- HospitalAtHome.App.FrontEnd/Pages/Medicals/RemoveMedical.cshtml.cs
+++ HospitalAtHome.App.FrontEnd/Pages/Medicals/RemoveMedical.cshtml.cs
@@ -1,3 +1,5 @@
+using HospitalAtHome.App.Controller;
+using HospitalAtHome.App.Model.AppRepository.RepMedical;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +7,25 @@ namespace HospitalAtHome.App.FrontEnd.Pages
 {
     public class RemoveMedicalModel : PageModel
     {
-        public void OnGet()
+        private readonly IRepositoryMedical iRepositoryMedical;
+        public Medical medical { get; set; }
+        public RemoveMedicalModel(IRepositoryMedical iRepositoryMedical)
         {
+            this.iRepositoryMedical = iRepositoryMedical;
+        }
+        public void OnGet(int code)
+        {
+            medical= iRepositoryMedical.getMedical(code);
+        }
+        public IActionResult OnPost(int code)
+        {
+            try{
+                iRepositoryMedical.removeMedical(code);
+                return RedirectToPage("./ListMedicals");
+            }
+            catch{
+                return RedirectToPage("../Error");
+            }
         }
     }
 }
---
+++ HospitalAtHome.App.Model/Migrations/20220520011216_FourM.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HospitalAtHome.App.Model.Migrations
+{
+    public partial class FourM : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Code",
+                table: "Persons",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Code",
+                table: "Persons",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+    }
+}
